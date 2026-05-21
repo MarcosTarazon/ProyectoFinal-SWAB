737c543250c4c351e3b9a2023bc835aa2aa0ae1c
marcosTaa07 <marcostrzn07@hotmail.com>
2026-05-21 08:26:50 -0700

    actualización de api, se configuro todo para DB context y base de datos con Sql

---
+++ Barberia.Api/Barberia.Api/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Barberia.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Barberia.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Servicio> Servicios { get; set; }
+        public DbSet<Barbero> Barberos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Cita> Citas { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+    }
+}
