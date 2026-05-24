5a5f4c895ea84dc97690f574b7ffc14842b693d4
Alezzz123 <aletzlizarraga1509@gmail.com>
2026-05-23 20:05:04 -0700

    feat: habilitar CORS para React (Vite) en Program.cs

--- Barberia.Api/Barberia.Api/Program.cs
+++ Barberia.Api/Barberia.Api/Program.cs
@@ -6,9 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowVite", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -19,6 +28,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowVite");
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
